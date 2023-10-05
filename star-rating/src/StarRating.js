463728706e3badb7a9ad67503589b1715c512ff3
ds3309 <d.suzuki.3309@gmail.com>
2023-10-06 06:24:29 +0900

    StarRatingコンポーネント追加

---
+++ star-rating/src/StarRating.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+
+const Star = ({ selected = false, onSelect = f => f }) => (
+    <FaStar color={selected ? "red" : "grey "} onClick={onSelect} />
+);
+
+export default function StarRating({ totalStars = 5 }) {
+    const [selectedStars, setSelectedStars] = useState(3);
+    return (
+        <>
+            {[...Array(totalStars)].map((_, i) => (
+                <Star
+                    key={i}
+                    selected={selectedStars > i}
+                    onSelect={() => setSelectedStars(i + 1)}
+                />
+            ))}
+            <p>
+                {selectedStars} of {totalStars} stars
+            </p>
+        </>
+    );
+} 
