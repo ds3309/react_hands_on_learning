bae46a0fa041148ff7fef2693990ec9f5aa08e48
ds3309 <d.suzuki.3309@gmail.com>
2023-10-13 06:10:49 +0900

    コンテキスト対応

--- star-rating/src/index.js
+++ star-rating/src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { createContext } from 'react';
+import colors from './color-data.json';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+export const ColorContext = createContext();
+
+render(
+  <ColorContext.Provider value={{ colors }}>
     <App />
-  </React.StrictMode>
+  </ColorContext.Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
